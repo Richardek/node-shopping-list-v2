b94dd06d1e0ba91ac33ef97b68f4c1c69870f74d
Richard McBride <richgametv@gmail.com>
2019-01-14 23:53:31 -0500

    change server file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
@@ -44,6 +43,22 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   }
 
   const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
+
+app.post('/recipes', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      //const message = `Missing \`${field}\` in request body`
+      const message = `Missing ${field} in a request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
 });
 
